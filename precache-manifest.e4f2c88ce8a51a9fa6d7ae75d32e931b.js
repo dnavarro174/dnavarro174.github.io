6aef2169b0a120d0bf6e36080d645bf88f577758
Dany <dnavarro174@gmail.com>
2019-07-16 21:02:53 -0500

    Primer commit
    Primeras modificaciones. 16/07/2019 09:02 pm

---
+++ precache-manifest.e4f2c88ce8a51a9fa6d7ae75d32e931b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71b6c1badd847044cd62c58c762c1502",
+    "url": "/index.html"
+  },
+  {
+    "revision": "76af03f52a7fe8379200",
+    "url": "/static/css/main.0010486f.chunk.css"
+  },
+  {
+    "revision": "c919152e9594a3e1104d",
+    "url": "/static/js/2.0a205d6d.chunk.js"
+  },
+  {
+    "revision": "76af03f52a7fe8379200",
+    "url": "/static/js/main.f0d6f855.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
